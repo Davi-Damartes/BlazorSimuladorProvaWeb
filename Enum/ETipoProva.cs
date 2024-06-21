0874211ffa51501fd966d6d2c74895c81342cd4a
Davi Lima <davidamartesgit@Gmail.com>
2024-06-21 19:21:58 -0300

    Enum para representar os tipo de Provas disponíveis

---
+++ Enum/ETipoProva.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorSimuladorProva.Enum
+{
+    public enum ETipoProva
+    {
+        Matematica = 1,
+        Portugues = 2,
+        Historia = 3,
+        Geografia = 4,
+        Ciencias = 5,
+        Fisica = 6,
+        Quimica = 7,
+        Biologia = 8,
+        Ingles = 9,
+        Artes = 10,
+        Educacao_Fisica = 11
+    }
+}
