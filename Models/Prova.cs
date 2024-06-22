d2389dab43f80c4e99e0b9ee544fb16309023491
Davi Lima <davidamartesgit@Gmail.com>
2024-06-22 16:27:23 -0300

    Add validações com DataAnnotations

--- Models/Prova.cs
+++ Models/Prova.cs
@@ -1,15 +1,23 @@
 ﻿using BlazorSimuladorProva.Enum;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorSimuladorProva.Models
 {
     public class Prova
     {
+        [MaxLength(150, ErrorMessage ="Nome deve ter no máximo 150 caracteres")]
         public string NomeProfessor {  get; set; } = string.Empty;
-        public int QuantiadadeAlunos {  get; set; } 
+
+        [MaxLength(30, ErrorMessage = "Quantida de Alunos deve ter no máximo 30")]
+        public int QuantidadeAlunos {  get; set; } 
+
         public ETipoProva TipoProva {  get; set; }
+
+        [MinLength(3, ErrorMessage = "Tem q ter no mínimo 3 pergutas")]
+        [MaxLength(10, ErrorMessage = "Tem q ter no mínimo 10 pergutas")]
         public int QuantidadePerguntas { get; set; }
 
-        public DateTime HoraEnvio { get; set; }
+        public DateTime HoraEnvio { get; set; } = DateTime.Now;
 
         public List<Perguntas> Perguntas { get; set; } = new List<Perguntas>();
     }
