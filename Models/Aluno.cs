b728698b50b1a8e2f345592d8c7eb5e7cc00d92b
Davi Lima <davidamartesgit@Gmail.com>
2024-06-23 18:33:39 -0300

    Add DataAnnotations para validação

--- Models/Aluno.cs
+++ Models/Aluno.cs
@@ -1,8 +1,14 @@
-﻿namespace BlazorSimuladorProva.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorSimuladorProva.Models
 {
     public class Aluno
     {
+        [Required]
         public string Nome { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
 
     }
