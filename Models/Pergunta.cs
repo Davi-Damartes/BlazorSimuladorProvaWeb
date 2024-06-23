6fb64d71ff688ac2478af9e63c9095650aac05dc
Davi Lima <davidamartesgit@Gmail.com>
2024-06-23 18:34:04 -0300

    Classe responsável por guardar as perguntas

---
+++ Models/Pergunta.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorSimuladorProva.Models
+{
+    public class Pergunta
+    {
+        public string Imagem { get; set; } = string.Empty;
+        public List<string> Questoes { get; set; } = new List<string>();
+
+        public List<string> Respostas { get; set; } = new List<string>();
+        public string RespostaCorreta { get; set; } = string.Empty;
+
+
+    }
+}
