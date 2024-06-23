fe145113071f90365ab1a6c3de46272b554fb1da
Davi Lima <davidamartesgit@Gmail.com>
2024-06-23 18:34:21 -0300

    Add DataAnnotations e validações

--- Models/Prova.cs
+++ Models/Prova.cs
@@ -5,20 +5,22 @@ namespace BlazorSimuladorProva.Models
 {
     public class Prova
     {
-        [MaxLength(150, ErrorMessage ="Nome deve ter no máximo 150 caracteres")]
+        [Required(ErrorMessage ="Campo obrigatório!")]
+        [MaxLength(100, ErrorMessage ="Nome deve ter no máximo 150 caracteres")]
         public string NomeProfessor {  get; set; } = string.Empty;
 
-        [MaxLength(30, ErrorMessage = "Quantida de Alunos deve ter no máximo 30")]
-        public int QuantidadeAlunos {  get; set; } 
-
+        [Required]
+        [Range(5, 30, ErrorMessage = "Número de 5 a 30 Alunos")]
+        public int QuantidadeAlunos {  get; set; }
+ 
         public ETipoProva TipoProva {  get; set; }
 
-        [MinLength(3, ErrorMessage = "Tem q ter no mínimo 3 pergutas")]
-        [MaxLength(10, ErrorMessage = "Tem q ter no mínimo 10 pergutas")]
+        [Required]
+        [Range(3,5, ErrorMessage = "Número de 3 a 5 Perguntas")]
         public int QuantidadePerguntas { get; set; }
 
         public DateTime HoraEnvio { get; set; } = DateTime.Now;
 
-        public List<Perguntas> Perguntas { get; set; } = new List<Perguntas>();
+        public List<Pergunta> Perguntas { get; set; } = new List<Pergunta>();
     }
 }
