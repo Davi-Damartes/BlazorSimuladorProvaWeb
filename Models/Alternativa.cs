998fde059bf7f931215997527ae656aaec691c3b
Davi Lima <davidamartesgit@Gmail.com>
2024-06-24 19:13:55 -0300

    Classe para guardar as alternaticas das Questoes

---
+++ Models/Alternativa.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorSimuladorProva.Models
+{
+    public class Alternativa
+    {
+        public string Texto { get; set; } = string.Empty;
+        public bool IsCorreta { get; set; }
+    }
+}
