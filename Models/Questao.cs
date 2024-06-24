e047dbccea4d7cefe481b24940cf51f0f3a0830f
Davi Lima <davidamartesgit@Gmail.com>
2024-06-24 19:14:17 -0300

    Add classe quer será responsável por guardar a Questao

---
+++ Models/Questao.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorSimuladorProva.Models
+{
+    public class Questao
+    {
+        [Required]
+        public string Enunciado { get; set; } = string.Empty;
+        public List<Alternativa> Alternativas { get; set; } = new List<Alternativa>();
+    }
+}
