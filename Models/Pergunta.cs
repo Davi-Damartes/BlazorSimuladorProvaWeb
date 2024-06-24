cf2b51b053b86b8cfcb4b166198ab35d05fca9f6
Davi Lima <davidamartesgit@Gmail.com>
2024-06-24 19:13:25 -0300

    Add lista usando a classe Questao

--- Models/Pergunta.cs
+++ Models/Pergunta.cs
@@ -3,10 +3,8 @@
     public class Pergunta
     {
         public string Imagem { get; set; } = string.Empty;
-        public List<string> Questoes { get; set; } = new List<string>();
-
-        public List<string> Respostas { get; set; } = new List<string>();
-        public string RespostaCorreta { get; set; } = string.Empty;
+        public List<Questao> Questoes { get; set; } = new List<Questao>();
+        public string Texto { get; set; } = string.Empty;
 
 
     }
