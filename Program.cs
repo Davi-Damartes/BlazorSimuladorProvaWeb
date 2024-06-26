cccbe423f73a6048e329b6de353ab069295c49e7
Davi Lima <davidamartesgit@Gmail.com>
2024-06-26 19:39:03 -0300

    Add Di do Service de Questao

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using BlazorSimuladorProva;
+using BlazorSimuladorProva.Service;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -7,7 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
+builder.Services.AddSingleton<QuestaoService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
