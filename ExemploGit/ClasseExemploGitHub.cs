111d49fed6352ceb5e4c2fedd7973c167a4609f1
Davi Lima <davidamartesgit@Gmail.com>
2024-06-27 11:23:13 -0300

    Commit Mensagem!!!

---
+++ ExemploGit/ClasseExemploGitHub.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorSimuladorProva.ExemploGit
+{
+    public class ClasseExemploGitHub
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Ola, mundo!");
+        }
+    }
+}
