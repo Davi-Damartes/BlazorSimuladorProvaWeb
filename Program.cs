6161e825f0caa31a068b204626a789f3d4d0966b
Davi Lima <davidamartesgit@Gmail.com>
2024-06-27 19:22:01 -0300

    Di do Prova Service

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddSingleton<QuestaoService>();
+builder.Services.AddSingleton<ProvaService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
