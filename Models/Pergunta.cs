2bf9db977cc3f9980e411d055308d5e8c93a20a4
Davi Lima <davidamartesgit@Gmail.com>
2024-06-28 19:27:48 -0300

    Adicionando campo para o Enunciado da Pergunta

--- Models/Pergunta.cs
+++ Models/Pergunta.cs
@@ -2,8 +2,8 @@
 {
     public class Pergunta
     {
+        public string EnunciadoDaPergunta { get;set; } = string.Empty;
         public List<Questao> Questoes { get; set; } = new List<Questao>();
-
 
     }
 }
