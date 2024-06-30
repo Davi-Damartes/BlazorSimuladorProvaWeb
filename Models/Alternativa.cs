bb229f002326ab2511be057ed3209a1792e23deb
Davi Lima <davidamartesgit@Gmail.com>
2024-06-30 16:44:42 -0300

    Add método para desabilitar botões

--- Models/Alternativa.cs
+++ Models/Alternativa.cs
@@ -8,7 +8,10 @@ namespace BlazorSimuladorProva.Models
         [Required(ErrorMessage = "Campo Alternativa é obrigatório")]
         [StringLength(400, ErrorMessage = "Número máximo de caracteres 400")]
         public string TextoDasAlternativa { get; set; } = string.Empty;
+
         public bool IsCorreta { get; set; }
+
+        public bool alternativaAdicionada { get; set; }
 
         public EOpcaoAlternativa OpcaoAlternativa { get; set; }
     }
