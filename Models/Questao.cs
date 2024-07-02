e5c83780a27b9b2fc1fa82a68813429d5136e545
Davi Lima <davidamartesgit@Gmail.com>
2024-07-02 19:47:57 -0300

    Removendo campo desnecessário

--- Models/Questao.cs
+++ Models/Questao.cs
@@ -1,12 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace BlazorSimuladorProva.Models
+﻿namespace BlazorSimuladorProva.Models
 {
     public class Questao
     {
-        [Required(ErrorMessage ="Campo Enunciado é obrigatório")]
-        public string EnunciadoQuestao { get; set; } = string.Empty;
-        public List<Alternativa> ListaAlternativas { get; set; } = new List<Alternativa>();
+     public List<Alternativa> ListaAlternativas { get; set; } = new List<Alternativa>();
 
     }
 }
