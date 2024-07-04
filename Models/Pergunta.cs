8c6210c0121caeade8bdb41028db5177fc5319a8
Davi Lima <davidamartesgit@Gmail.com>
2024-07-04 19:30:22 -0300

    Removendo possibilidade de null no campo

--- Models/Pergunta.cs
+++ Models/Pergunta.cs
@@ -2,7 +2,7 @@
 {
     public class Pergunta
     {
-        public int? NumeroPergunta { get;set; }
+        public int NumeroPergunta { get;set; }
         public string EnunciadoDaPergunta { get;set; } = string.Empty;
         public List<Questao> Questoes { get; set; } = new List<Questao>();
 
