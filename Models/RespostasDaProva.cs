552e36c9e74c3e63e21cc585661d11edc738cd63
Davi Lima <davidamartesgit@Gmail.com>
2024-07-04 19:38:34 -0300

    Classe para guardar as reposta que forem geradas nas provas

---
+++ Models/RespostasDaProva.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorSimuladorProva.Models
+{
+    public class RespostasDaProva
+    {
+        public List<int> NumeroPergunta { get; set; } = new List<int>();
+
+        public int QntAcertos { get; set; } = 0;
+        
+    }
+}
