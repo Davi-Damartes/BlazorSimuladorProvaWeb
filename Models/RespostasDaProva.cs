c6dee3a52784ac7df1595803af9766080d305d63
Davi Lima <davidamartesgit@Gmail.com>
2024-07-05 19:16:24 -0300

    Add propriedades para respostas das Perguntas

--- Models/RespostasDaProva.cs
+++ Models/RespostasDaProva.cs
@@ -1,10 +1,17 @@
-﻿namespace BlazorSimuladorProva.Models
+﻿using BlazorSimuladorProva.Enum;
+
+namespace BlazorSimuladorProva.Models
 {
     public class RespostasDaProva
     {
         public List<int> NumeroPergunta { get; set; } = new List<int>();
 
         public int QntAcertos { get; set; } = 0;
-        
+
+        public List<bool> Acertou { get; set; } = new List<bool>();
+
+        public List<Pergunta> PerguntasCertas { get; set; } = new List<Pergunta>();
+
+        public EOpcaoAlternativa RespostaSelecionada { get; set; }
     }
 }
