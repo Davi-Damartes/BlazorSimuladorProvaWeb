f9188b30f8d0123dfd8d6f32d2fc120d3ca822ba
Davi Lima <davidamartesgit@Gmail.com>
2024-07-07 17:29:20 -0300

    Removendo campos

--- Models/RespostasDaProva.cs
+++ Models/RespostasDaProva.cs
@@ -4,14 +4,11 @@ namespace BlazorSimuladorProva.Models
 {
     public class RespostasDaProva
     {
-        public List<int> NumeroPergunta { get; set; } = new List<int>();
+        public int NumeroPergunta { get; set; } 
 
-        public int QntAcertos { get; set; } = 0;
-
-        public List<bool> Acertou { get; set; } = new List<bool>();
-
-        public List<Pergunta> PerguntasCertas { get; set; } = new List<Pergunta>();
+        public bool AcertouPergunta { get; set; }
 
         public EOpcaoAlternativa RespostaSelecionada { get; set; }
     }
+
 }
