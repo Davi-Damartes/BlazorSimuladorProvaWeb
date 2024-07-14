c36adc1169122aae178c392234fbc64d1662a72f
Davi Lima <davidamartesgit@Gmail.com>
2024-07-14 16:04:05 -0300

    Add propriedade para guardar as alternativas corretas

--- Models/Prova.cs
+++ Models/Prova.cs
@@ -5,18 +5,18 @@ namespace BlazorSimuladorProva.Models
 {
     public class Prova
     {
-        [Required(ErrorMessage ="Campo obrigatório!")]
-        [MaxLength(100, ErrorMessage ="Nome deve ter no máximo 150 caracteres")]
-        public string NomeProfessor {  get; set; } = string.Empty;
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [MaxLength(100, ErrorMessage = "Nome deve ter no máximo 150 caracteres")]
+        public string NomeProfessor { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Campo Quantidade de Alunos é obrigatório")]
         [Range(5, 30, ErrorMessage = "Número de 5 a 30 Alunos")]
-        public int QuantidadeAlunos {  get; set; }
- 
-        public ETipoProva TipoProva {  get; set; }
+        public int QuantidadeAlunos { get; set; }
+
+        public ETipoProva TipoProva { get; set; }
 
         [Required(ErrorMessage = "Campo Quantidade Perguntas é obrigatório")]
-        [Range(3,5, ErrorMessage = "Número de 3 a 5 Perguntas")]
+        [Range(3, 5, ErrorMessage = "Número de 3 a 5 Perguntas")]
         public int QuantidadePerguntas { get; set; }
 
         public int QuantidadeAcertos { get; set; }
@@ -27,5 +27,7 @@ namespace BlazorSimuladorProva.Models
 
         public DateTime HoraEnvio { get; set; }
         public List<Pergunta> Perguntas { get; set; } = new List<Pergunta>();
+
+        public List<int> AlternativasCorretas { get; set; } = new List<int>();
     }
 }
