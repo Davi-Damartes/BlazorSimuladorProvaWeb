cf5ccba6c8669de5ba4333f1cf7f30251259a38e
Davi Lima <davidamartesgit@Gmail.com>
2024-07-14 15:59:17 -0300

    Add propriedade

--- Service/ProvaService.cs
+++ Service/ProvaService.cs
@@ -5,7 +5,5 @@ namespace BlazorSimuladorProva.Service
     public class ProvaService
     {
         public Prova ProvaInstancia { get; set; } = new();
-        public int PerguntaAtual { get; set; } = 0;
-
     }
 }
