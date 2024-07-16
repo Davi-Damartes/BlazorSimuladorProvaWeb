3862f08517482952d034ac543f1d9bf164154625
Davi Lima <davidamartesgit@Gmail.com>
2024-07-16 18:45:50 -0300

    Deploy

--- Program.cs
+++ Program.cs
@@ -13,6 +13,6 @@ builder.Services.AddSingleton<ProvaService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 
-
+ 
 
 await builder.Build().RunAsync();
