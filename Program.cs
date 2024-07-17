7af74ad483a29735e9b2107585bb3b7c544a811b
Davi Lima <davidamartesgit@Gmail.com>
2024-07-17 14:51:21 -0300

    Adicionando icone aba

--- Program.cs
+++ Program.cs
@@ -13,6 +13,5 @@ builder.Services.AddSingleton<ProvaService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 
- 
 
 await builder.Build().RunAsync();
